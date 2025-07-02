8d0034e87e8cc70e46e217085b91c6e4ca8fd993
arielKen <ariel.kenigs@gmail.com>
2025-07-02 19:23:36 +0300

    Login function completed with database seperation

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/ConnectedUser.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/ConnectedUser.java
@@ -2,9 +2,16 @@ package il.cshaifasweng.OCSFMediatorExample.entities;
 
 public class ConnectedUser {
     String Username;
+    int UserID;
+    int creditId;
+    String userType;
 
-
-    public ConnectedUser(String username) {this.Username=username;}
+    public ConnectedUser(String username,int UserId,int creditId,String userType) {
+        this.Username=username;
+        this.UserID=UserId;
+        this.creditId=creditId;
+        this.userType=userType;
+    }
 
 
     public String getUsername() {
